a9e6f209ab3a3f910c641dbdf4dd21753d5d68cf
Zeamanuel-Admasu <zeamanshibru2020@gmail.com>
2025-07-25 14:46:28 +0300

    yo

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import list_books, LibraryDetailView, login_view, logout_view, register
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import add_book, edit_book, delete_book
+
 from . import views
 urlpatterns = [
     path('books/', list_books, name='list_books'),
@@ -19,4 +21,8 @@ urlpatterns = [
     path('admin-dashboard/', views.admin_view, name='admin_view'),
     path('librarian-dashboard/', views.librarian_view, name='librarian_view'),
     path('member-dashboard/', views.member_view, name='member_view'),
+
+    path('books/add/', add_book, name='add_book'),
+    path('books/<int:pk>/edit/', edit_book, name='edit_book'),
+    path('books/<int:pk>/delete/', delete_book, name='delete_book'),
 ]
