3e24a0ac3f6d788f2981996a2dd704b5da1eb382
Zeamanuel-Admasu <zeamanshibru2020@gmail.com>
2025-08-03 14:49:26 +0300

    pp

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import BookList
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),  # ✅ This connects your app's URLs
+    path('books/', BookList.as_view(), name='book-list'),
 ]
