6930542cd69ee79af2443f1b3c782febf47d8375
Zeamanuel-Admasu <zeamanshibru2020@gmail.com>
2025-08-03 11:54:14 +0300

    pp

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,7 +1,6 @@
 from django import forms
-from .models import Book
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'publication_year']
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, label='Your Name')
+    email = forms.EmailField(label='Email Address')
+    message = forms.CharField(widget=forms.Textarea, label='Message')
